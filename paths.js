feaf44f57fb24e393d87834758fd0e9ee3679c15
niccolovlnt <128193896+niccolovlnt@users.noreply.github.com>
2023-07-20 09:00:40 +0200

    c

--- paths.js
+++ paths.js
@@ -3,11 +3,12 @@ const ObjectId=require('mongodb').ObjectId;
 //const auth=require('./auth').auth
 const crypto=require('crypto')
 var cors=require('cors')
-const express=require('express')
+const express=require('express');
 const path=require('path');
 const { MongoClient } = require('mongodb');
 const mongo="mongodb+srv://volonteniccolo:olCHDtzs1wtnYLEl@pwm.yfvispg.mongodb.net/?retryWrites=true&w=majority"
-const app=express()
+const app=express();
+
 app.use(cors())
 app.use(express.json())
 function hash(input) {
@@ -27,34 +28,27 @@ app.get('/', function (req, res) {
 });
 
 async function addUser(res, user) {
-    if (user.name == undefined) {
-        res.status(400).send("Missing Name")
+    if (user.username == undefined) {
+        res.status(400).send("Missing username")
         return
     }
-    if (user.surname == undefined) {
-        res.status(400).send("Missing Surname")
-        return
-    }
-    if (user.email == undefined) {
+    if (user.mail == undefined) {
         res.status(400).send("Missing Email")
         return
     }
-    if (user.password == undefined || user.password.length < 3) {
+    if (user.pass == undefined || user.pass.length < 3) {
         res.status(400).send("Password is missing or too short")
         return
     }
-    if (user.date == undefined) {
-        res.status(400).send("Date is missing or too short")
+    if(user.genres == undefined){
+        res.status(400).send("Genres not chosen")
         return
     }
-
-    user.password = hash(user.password)
-
-    var pwmClient = await new mongoClient(uri).connect()
+    user.pass = hash(user.pass)
+    var pwmClient = await new mongoClient(mongo).connect()
     try {
-        var items = await pwmClient.db("pwm").collection('users').insertOne(user)
+        var items = await pwmClient.db("RhythmHub").collection('Users').insertOne(user)
         res.json(items)
-
     }
     catch (e) {
         console.log('catch in test');
@@ -65,9 +59,43 @@ async function addUser(res, user) {
         res.status(500).send(`Errore generico: ${e}`)
     };
 }
-app.post("/users", function (req, res) {
+app.post("/users", function (req, res){
     addUser(res, req.body)
 })
+
+app.post("/login", async (req,res)=>{
+    login=req.body
+    if (login.email == undefined) {
+        res.status(400).send("Missing Email")
+        return
+    }
+    if (login.pass == undefined) {
+        res.status(400).send("Missing Password")
+        return
+    }
+    login.pass=hash(login.pass)
+    var pwmClient = await new mongoClient(mongo).connect()
+    var filter = {
+        $and: [
+            { "mail": login.mail },
+            { "pass": login.pass }
+        ]
+    }
+    var loggedUser = await pwmClient.db("RhythmHub")
+        .collection('Users')
+        .findOne(filter);
+    console.log(loggedUser)
+    if (loggedUser == null) {
+        res.status(401).send("Unauthorized")
+    } else {
+        res.json(loggedUser)
+    }
+})
+
+
+
+
+
 app.listen(3000, "0.0.0.0", ()=>{
     console.log("Server Initialized")
 })
